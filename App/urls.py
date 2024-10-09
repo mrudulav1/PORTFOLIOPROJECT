6628e0ac7ee955bfaff7c9dd1e3077d4e448f81d
mrudula <mrudulavaji11@gmail.com>
2024-10-09 16:20:20 +0530

    new updations

--- App/urls.py
+++ App/urls.py
@@ -1,12 +1,10 @@
+from django.urls import path
 from App import views
-from django.urls import path,include
+
+
 
 urlpatterns = [
-    path('',views.index,name="index"),
-    path('contact',views.contact,name="contact"),
-    path('about',views.about,name="about"),
-
-
+  path('',views.index,name='index'),
 ]
 
 
